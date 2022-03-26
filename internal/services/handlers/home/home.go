ee35a30ca30d877336158fefa814fda0070ed0d3
herb <ghstahl@gmail.com>
2022-03-26 09:14:42 -0700

    minor cleanup

--- internal/services/handlers/home/home.go
+++ internal/services/handlers/home/home.go
@@ -7,10 +7,7 @@ import (
 
 	"reflect"
 
-	auth_shared "echo-starter/internal/contracts/auth/shared"
 	"echo-starter/internal/models"
-	"echo-starter/internal/session"
-	"encoding/json"
 	"net/http"
 
 	contracts_core_claimsprincipal "github.com/fluffy-bunny/grpcdotnetgo/pkg/contracts/claimsprincipal"
@@ -46,34 +43,10 @@ func (s *service) GetMiddleware() []echo.MiddlewareFunc {
 	return []echo.MiddlewareFunc{}
 }
 func (s *service) Do(c echo.Context) error {
-	test := &struct {
-		Bob string
-	}{
-		Bob: "bob",
-	}
-	sess := session.GetSession(c)
-	jsonProfile, _ := sess.Values[auth_shared.ProfileSessionKey]
-	if jsonProfile != nil {
-		var profile map[string]interface{}
-		json.Unmarshal(jsonProfile.([]byte), &profile)
-		//	jsonProfileS := utils.PrettyJSON(profile)
-		user := &models.User{
-			Name: profile["name"].(string),
-			ID:   profile["sub"].(string),
-		}
 
-		return c.Render(http.StatusOK, "content", map[string]interface{}{
-			"user":   user,
-			"paths":  models.NewPaths(),
-			"test":   test,
-			"claims": s.ClaimsPrincipal.GetClaims(),
-		})
-		//	return c.String(http.StatusOK, jsonProfileS)
-	}
 	return c.Render(http.StatusOK, "content", map[string]interface{}{
 		"isAuthenticated": func() bool { return s.ClaimsPrincipal.HasClaimType(wellknown.ClaimTypeAuthenticated) },
 		"paths":           models.NewPaths(),
-		"test":            test,
 		"claims":          s.ClaimsPrincipal.GetClaims(),
 	})
 }
