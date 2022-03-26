bd19b86e74b258214f0fde7aa06e51eb677348f2
herb <ghstahl@gmail.com>
2022-03-26 12:28:59 -0700

    cleanup

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,13 +1,8 @@
 package main
 
 import (
+	"echo-starter/internal/templates"
 	"fmt"
-	"io"
-	"io/ioutil"
-	"os"
-	"path/filepath"
-	"strings"
-	"text/template"
 
 	contracts_config "echo-starter/internal/contracts/config"
 	services_container "echo-starter/internal/services/container"
@@ -34,42 +29,6 @@ import (
 
 var version = "Development"
 
-type TemplateRenderer struct {
-	templates *template.Template
-}
-
-func (t *TemplateRenderer) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
-	return t.templates.ExecuteTemplate(w, name, data)
-}
-func findAndParseTemplates(rootDir string, funcMap template.FuncMap) (*template.Template, error) {
-	cleanRoot := filepath.Clean(rootDir)
-	pfx := len(cleanRoot) + 1
-	root := template.New("")
-
-	err := filepath.Walk(cleanRoot, func(path string, info os.FileInfo, e1 error) error {
-		if !info.IsDir() && strings.HasSuffix(path, ".tpl") {
-			if e1 != nil {
-				return e1
-			}
-
-			b, e2 := ioutil.ReadFile(path)
-			if e2 != nil {
-				return e2
-			}
-
-			name := path[pfx:]
-			t := root.New(name).Funcs(funcMap)
-			_, e2 = t.Parse(string(b))
-			if e2 != nil {
-				return e2
-			}
-		}
-
-		return nil
-	})
-
-	return root, err
-}
 func main() {
 	appInstanceID := uuid.New().String()
 
@@ -99,13 +58,7 @@ func main() {
 
 	e := echo.New()
 	//Set Renderer
-	t, err := findAndParseTemplates("./templates", nil)
-
-	renderer := &TemplateRenderer{
-		//	templates: template.Must(template.ParseGlob("templates/*.tpl")),
-		templates: t,
-	}
-	e.Renderer = renderer
+	e.Renderer = templates.GetTemplateRender("./templates")
 
 	if core_utils.IsEmptyOrNil(appConfig.SessionKey) {
 		fmt.Println("WARNING: SESSION_KEY must be set for production......")
--- internal/templates/render.go
+++ internal/templates/render.go
@@ -3,15 +3,69 @@ package templates
 import (
 	"echo-starter/internal/models"
 	"echo-starter/internal/wellknown"
+	"html/template"
+	"io"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"strings"
 
 	contracts_core_claimsprincipal "github.com/fluffy-bunny/grpcdotnetgo/pkg/contracts/claimsprincipal"
 
 	"github.com/labstack/echo/v4"
 )
 
+func GetTemplateRender(rootDir string) *TemplateRenderer {
+	t, err := FindAndParseTemplates(rootDir, nil)
+	if err != nil {
+		panic(err)
+	}
+
+	return &TemplateRenderer{
+		templates: t,
+	}
+}
+
 func Render(c echo.Context, claimsPrincipal contracts_core_claimsprincipal.IClaimsPrincipal, code int, name string, data map[string]interface{}) error {
 	data["isAuthenticated"] = func() bool { return claimsPrincipal.HasClaimType(wellknown.ClaimTypeAuthenticated) }
 	data["paths"] = models.NewPaths()
 	return c.Render(code, name, data)
 
+}
+
+type TemplateRenderer struct {
+	templates *template.Template
+}
+
+func (t *TemplateRenderer) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+	return t.templates.ExecuteTemplate(w, name, data)
+}
+func FindAndParseTemplates(rootDir string, funcMap template.FuncMap) (*template.Template, error) {
+	cleanRoot := filepath.Clean(rootDir)
+	pfx := len(cleanRoot) + 1
+	root := template.New("")
+
+	err := filepath.Walk(cleanRoot, func(path string, info os.FileInfo, e1 error) error {
+		if !info.IsDir() && strings.HasSuffix(path, ".tpl") {
+			if e1 != nil {
+				return e1
+			}
+
+			b, e2 := ioutil.ReadFile(path)
+			if e2 != nil {
+				return e2
+			}
+
+			name := path[pfx:]
+			t := root.New(name).Funcs(funcMap)
+			_, e2 = t.Parse(string(b))
+			if e2 != nil {
+				return e2
+			}
+		}
+
+		return nil
+	})
+
+	return root, err
 }
