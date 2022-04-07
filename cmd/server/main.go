59fbc61ccb16e8e4da757ce61f1d33f701b2544a
herb <ghstahl@gmail.com>
2022-04-07 13:47:17 -0700

    unit tests

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,8 +3,9 @@ package main
 import (
 	"fmt"
 
-	"github.com/fluffy-bunny/grpcdotnetgo/pkg/echo/runtime"
+	"echo-starter/internal/startup"
 
+	"github.com/fluffy-bunny/grpcdotnetgo/pkg/echo/runtime"
 	"github.com/rs/zerolog/log"
 )
 
@@ -13,7 +14,7 @@ var version = "Development"
 func main() {
 	fmt.Println("Version:" + version)
 	DumpPath("./")
-	r := runtime.New(NewStartup())
+	r := runtime.New(startup.NewStartup())
 	err := r.Run()
 	if err != nil {
 		log.Fatal().Err(err).Msg("Failed to run the application")
